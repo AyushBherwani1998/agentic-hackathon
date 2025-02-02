3df857f445513fa46e62e502a16a6ec715d16a33
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-02-02 08:46:37 +0530

    add privy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { login, logout, signMessage } = usePrivy();
+  const { wallets } = useWallets();
 
   return (
     <>
@@ -18,8 +21,22 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => login()}>Login</button>
+        <button onClick={() => logout()}> Logout </button>
+        <button
+          onClick={async () => {
+            const wallet = wallets[0];
+            const provider = await wallet.getEthereumProvider();
+            const address = wallet.address;
+            const message = "Hello from Privy";
+            const signature = await provider.request({
+              method: "personal_sign",
+              params: [message, address],
+            });
+            console.log(signature);
+          }}
+        >
+          sign message
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +46,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { PrivyProvider } from "@privy-io/react-auth";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrivyProvider
+      appId={import.meta.env.VITE_PRIVY_APP_ID}
+      config={{
+        loginMethods: ["email", "google", "wallet"],
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </StrictMode>
+);
