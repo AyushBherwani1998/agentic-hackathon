d64178ffac4e5a2d863e478dcafa67392593a801
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-02-09 17:37:29 +0530

    fix copy address

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 export default function Header() {
   const { logout, ready } = usePrivy();
   const { wallets } = useWallets();
+  const [buttonText, setButtonText] = useState("");
 
   const [isReady, setIsReady] = useState(false);
   useEffect(() => {
     const init = async () => {
       setIsReady(ready);
+      setButtonText(shortenAddress(wallets[0].address));
     };
     init();
   }, [wallets]);
@@ -30,8 +32,15 @@ export default function Header() {
       <div className="flex flex-row space-x-2">
         {isReady && (
           <SecondaryButton
-            text={shortenAddress(wallets[0].address)}
+            text={buttonText}
             icon={"active.svg"}
+            onClick={() => {
+              navigator.clipboard.writeText(wallets[0].address);
+              setButtonText("Copied");
+              setTimeout(() => {
+                setButtonText(shortenAddress(wallets[0].address));
+              }, 500);
+            }}
           />
         )}
         <SecondaryButton text="Logout" onClick={logout} />
