1fb933373ab9b7aa757a5ceaba23369ed9b5bc9b
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-02-09 21:39:38 +0530

    fix session creation

--- frontend/src/services/utils/session.ts
+++ frontend/src/services/utils/session.ts
@@ -10,6 +10,12 @@ import { odysseyTestnet } from "viem/chains";
 const getRandomSalt = (max: number, min: number) => {
   return Math.random() * (max - min) + min;
 };
+
+const genRanHex = (size: number) =>
+  [...Array(size)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
+
 export const createSession = (
   sessionOwner: `0x${string}`,
   actionTarget: `0x${string}`,
@@ -21,7 +27,7 @@ export const createSession = (
       threshold: 1,
       owners: [sessionOwner],
     }),
-    salt: toHex(toBytes(getRandomSalt(999, 100).toString(), { size: 32 })),
+    salt: toHex(genRanHex(32)),
     erc7739Policies: {
       allowedERC7739Content: [],
       erc1271Policies: [],
